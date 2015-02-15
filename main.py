1a391ee78b868ba895a51acd8712a06cf2a730e5
Peter Lesslie <pclesslie@gmail.com>
2015-02-15 16:52:42 -0600

    Refactor code out of main function

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
 from optparse import OptionParser
 import sys
 
-if __name__ == '__main__':
-    parser = OptionParser()
-    parser.add_option('--filename', dest='filename', default='input.txt', help='input file')
-    parser.add_option('--dict', dest='dictionary', default='/usr/share/dict/words', help='dictionary file to use for words')
-    (options, args) = parser.parse_args()
-    print('Opening: %s' % options.filename)
+def readInput(filename):
+    print('Opening: %s' % filename)
     board = []
-    with open(options.filename) as f:
-        s = [c.lower() for c in f.readline().split()]
+    with open(filename) as f:
+        def parseLine():
+            return [c.lower() for c in f.readline().split()]
+        s = parseLine()
         board.append(s)
         dim = len(s)
         for i in range(1, dim):
-            s = [c.lower() for c in f.readline().split()]
+            s = parseLine()
             if len(s) != dim:
-                raise ValueError('Input file format not correct!')
+                raise ValueError('Input file format not correct.')
             board.append(s)
         s = f.readline().split()
         solutionSizes = [int(x) for x in s]
+    return board, dim, solutionSizes
+
+def printBoard(board):
     print('Board: ')
     for row in board:
         print row
-    print 'Solution sizes: ', solutionSizes
-    print('Dictionary file: %s' % options.dictionary)
+
+def loadDictionary(filename, dim):
     maxWordSize = dim*dim
     def validWord(word):
         if len(word) > maxWordSize:
             return False
         return word.strip().lower().isalpha()
-    dictionary = [line.strip().lower() for line in open(options.dictionary) if validWord(line)]
-    for word in dictionary:
-        print word
+    return [line.strip().lower() for line in open(filename) if validWord(line)]
 
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option('--filename', dest='filename', default='input.txt', help='input file')
+    parser.add_option('--dict', dest='dictionary', default='/usr/share/dict/words', help='dictionary file to use for words')
+    (options, args) = parser.parse_args()
+    board, dim, solutionSizes = readInput(options.filename)
+    printBoard(board)
+    print 'Solution sizes: ', solutionSizes
+    print('Dictionary file: %s' % options.dictionary)
+    dictionary = loadDictionary(options.dictionary, dim)
 
