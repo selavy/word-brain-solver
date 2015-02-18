0d65ca7dd63aa6339be6fd460126afcfd546dcf0
Peter Lesslie <pclesslie@gmail.com>
2015-02-18 15:36:14 -0600

    Filter dictionary more.
    Since we know the exact length of the words we are looking for
    and the letter frequencies available, filter the dictionary first
    so that lookups will be much faster.

--- main.py
+++ main.py
@@ -26,40 +26,16 @@ def printBoard(board):
     for row in board:
         print row
 
-def loadDictionary(filename, dim):
-    maxWordSize = dim*dim
+def loadDictionary(filename, solutionSizes):
     def validWord(word):
-        if len(word) > maxWordSize:
+        if len(word) not in solutionSizes:
             return False
         return word.strip().lower().isalpha()
     return [line.strip().lower() for line in open(filename) if validWord(line)]
 
-
 GameState = namedtuple('GameState', ['board', 'currPos', 'finishedWords', 'visited', 'currentWord'])
 HeapItem = namedtuple('HeapItem', ['priority', 'weight', 'heuristic', 'state'])
 Pos = namedtuple('Pos', ['x', 'y'])
-
-# Board
-#-------------
-#|0,0|0,1|0,2|
-#|-----------|
-#|1,0|1,1|1,2|
-#|-----------|
-#|2,0|2,1|2,2|
-#-------------
-def createInitialState(board):
-    dim = len(board)
-    return GameState(board=board, currPos=Pos(0, 0), finishedWords=[], visited=[[False for i in range(dim)] for i in range(dim)], currentWord='')
-
-def createIsFinished(expectedWords):
-    def isFinished(state):
-        visited = state.visited
-        for row in visited:
-            for cell in row:
-                if cell:
-                    return False
-        return len(state.finishedWords) == len(expectedWords)
-    return isFinished
 
 def createCheckCurrentWord(dictionary, solutionSizes):
     def checkCurrentWord(state):
@@ -68,64 +44,35 @@ def createCheckCurrentWord(dictionary, solutionSizes):
         return False
     return checkCurrentWord
 
-def createGenerateMoves(checkCurrentWord):
-    def generateMoves(state):
-        if checkCurrentWord(state):
-            state.finishedWords.append(state.currentWord)
-            state.currentWord = ''
-        return []
-    return generateMoves
-
-
-def AStarSearch(start, goal, moves, heuristic):
-    Node = collections.namedtuple('Node', ['f', 'state', 'pred', 'move', 'g', 'h'])
-    SAW = collections.namedtuple('SAW', ['state', 'action', 'weight'])
-    Q = []
-    def nodeToSAW(saw, prev):
-        h = heuristic(saw.state)
-        if not prev:
-            g = saw.weight
+def letterFrequencies(word):
+    freq = {}
+    for letter in word:
+        if letter in freq:
+            freq[letter] += 1
         else:
-            g = prev.g + saw.weight
-        f = g + h
-        return Node(f=f, state=saw.state, prev=prev, move=saw.action, g=g, h=h)
-    for x in start:
-        h = heuristic(x)
-        heapq.heappush(Q, Node(f=h, state=x, pred=None, move=None, g=0, h=h))
-    while heap:
-        curr = heapq.heappop(heap)
-        if goal(curr.state):
-            print('Solution!')
-            sys.exit(0)
-        else:
-            possibleMoves = moves(curr.state)
-            for move in possibleMoves:
-                saw = nodeToSAW(move, curr)
-                heapq.heappush(heap, saw)
-            if len(heap) == 0:
-                print('No solution!')
-                sys.exit(0)
+            freq[letter] = 1
+    return freq
+ 
+def getLetterFrequencies(board):
+    freq = {}
+    for row in board:
+        for cell in row:
+            if cell in freq:
+                freq[cell] += 1
+            else:
+                freq[cell] = 1
+    return freq
 
-def solver(board, dictionary, solutionSizes):
-    def heuristic(state):
-        return 0
-    def goal(state):
-        if len(state.words) == len(solutionSizes):
-            return True
-        return False
-    USED = 'USED'
-    BLANK = 'NONE'
-    Pos = namedtuple('Pos', ['x', 'y'])
-    State = namedtuple('State', ['board', 'words', 'curr', 'pos'])
-    dim = len(boardce)
-    start = []
-    for x in range(dim):
-        for y in range(dim):
-            curr = board[x][y]
-            b = board
-            b[x][y] = USED
-            start.append(State(board=b, words=[], curr=curr, pos=Pos(x,y)))
-    print start
+def filterWordOnFrequencies(word, letters):
+    freq = letterFrequencies(word)
+    for letter in freq:
+        if letter not in letters:
+            return False
+        elif freq[letter] > letters[letter]:
+            return False
+        else:
+            continue
+    return True
 
 if __name__ == '__main__':
     parser = OptionParser()
@@ -133,26 +80,14 @@ if __name__ == '__main__':
     parser.add_option('--dict', dest='dictionary', default='/usr/share/dict/words', help='dictionary file to use for words')
     (options, args) = parser.parse_args()
     board, solutionSizes = readInput(options.filename)
+    letterFreq = getLetterFrequencies(board)
     printBoard(board)
+    print(letterFreq)
     print 'Solution sizes: ', solutionSizes
     print('Dictionary file: %s' % options.dictionary)
-# TODO (plesslie)
-# filter dictionary on solution sizes
-    dictionary = loadDictionary(options.dictionary, len(board))
-    solver(board, dictionary, solutionSizes)
-
-    # heap = [HeapItem(priority=1, weight=1, heuristic=0, state=createInitialState(board))]
-    # isFinished = createIsFinished(solutionSizes)
-    # checkCurrentWord = createCheckCurrentWord(dictionary, solutionSizes)
-    # generateMoves = createGenerateMoves(checkCurrentWord)
-    # while heap:
-    #     state = heapq.heappop(heap).state
-    #     print state
-    #     if isFinished(state):
-    #         print('Done words = %s' % str(state.finishedWords))
-    #         sys.exit(0)
-    #     moves = generateMoves(state)
-    #     for move in moves:
-    #         heapq.heappush(move)
-    # print('Heap is empty. No solution found!')
+    dictionary = loadDictionary(options.dictionary, solutionSizes)
+    print(len(dictionary))
+    dictionary = [x for x in dictionary if filterWordOnFrequencies(x, letterFreq)]
+    print(len(dictionary))
+    print(dictionary)
 
